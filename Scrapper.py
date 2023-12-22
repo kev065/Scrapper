934255b7c1ff96bab0c2be845a41ddd8a84387c5
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-12-22 14:38:38 +0300

    Scrapping well

--- Scrapper.py
+++ Scrapper.py
@@ -17,60 +17,60 @@ with open('webpage.html', 'w') as topics_list:
 from bs4 import BeautifulSoup
 
 doc = BeautifulSoup(page_content, 'html.parser')
-# print(type(doc))
-title_selection = "f3 lh-condensed mb-0 mt-1 Link--primary"
-topic_title_tags = doc.find_all('p', class_=title_selection)
-# print(topic_title_tags)
+# # print(type(doc))
+# title_selection = "f3 lh-condensed mb-0 mt-1 Link--primary"
+# topic_title_tags = doc.find_all('p', class_=title_selection)
+# # print(topic_title_tags)
 
-description = 'f5 color-fg-muted mb-0 mt-1'
-topic_description = doc.find_all('p', class_ = description)
-# print(topic_description)
+# description = 'f5 color-fg-muted mb-0 mt-1'
+# topic_description = doc.find_all('p', class_ = description)
+# # print(topic_description)
 
-link = "no-underline flex-grow-0"
-topic_link_tags = doc.find_all('a',class_ =link)
+# link = "no-underline flex-grow-0"
+# topic_link_tags = doc.find_all('a',class_ =link)
 
-topic1_url = "https://github.com" + topic_link_tags[1]['href']
-# print(topic1_url)
-# print(topic_title_tags[12].text)
+# topic1_url = "https://github.com" + topic_link_tags[1]['href']
+# # print(topic1_url)
+# # print(topic_title_tags[12].text)
 
-topic_titles = []
-for tag in topic_title_tags:
-    topic_titles.append(tag.text)
+# topic_titles = []
+# for tag in topic_title_tags:
+#     topic_titles.append(tag.text)
 
-# print(topic_titles[:5])
+# # print(topic_titles[:5])
 
-topic_desc = []
-for tag in topic_description:
-    topic_desc.append(tag.text.strip())
+# topic_desc = []
+# for tag in topic_description:
+#     topic_desc.append(tag.text.strip())
 
-# print(topic_desc[:5])
+# # print(topic_desc[:5])
 
-topic_URLs = []
+# topic_URLs = []
 base_url = "https://github.com"
-for tag in topic_link_tags:
-    topic_URLs.append(base_url+tag['href'])
-# print(topic_URLs[:5]) 
+# for tag in topic_link_tags:
+#     topic_URLs.append(base_url+tag['href'])
+# # print(topic_URLs[:5]) 
 
 import pandas as pd
 
-topics_dictionary = {
-    'Topic Title': topic_titles,
-    'Topic Description': topic_desc,
-    'Topic URLs': topic_URLs
-}
+# topics_dictionary = {
+#     'Topic Title': topic_titles,
+#     'Topic Description': topic_desc,
+#     'Topic URLs': topic_URLs
+# }
 
-topics_df = pd.DataFrame(topics_dictionary)
+# topics_df = pd.DataFrame(topics_dictionary)
 
-# print(topics_df)
+# # print(topics_df)
 
-topics_df .to_csv('Topics.csv')
+# topics_df .to_csv('Topics.csv')
 
-#########
+# #########
 
-topic_page_url = topic_URLs[0]
-# print(topic_page)
-response = requests.get(topic_page_url)
-# print(response.status_code)
+# topic_page_url = topic_URLs[0]
+# # print(topic_page)
+# response = requests.get(topic_page_url)
+# # print(response.status_code)
 
 
 
@@ -79,14 +79,14 @@ h3_selector = 'f3 color-fg-muted text-normal lh-condensed'
 repo_tags = topic_doc.find_all('h3', class_ = h3_selector)
 
 # print(len(repo_tags))
-# print(repo_tags[2])
-a_tags = repo_tags[0].find_all('a')
-# print(a_tags[0])
-username = a_tags[0].text.strip()
-# print(username)
-repo_name = a_tags[1].text.strip()
-# print(repo_name)
-repo_url = base_url + a_tags[1]['href']
+# # print(repo_tags[2])
+# a_tags = repo_tags[0].find_all('a')
+# # print(a_tags[0])
+# username = a_tags[0].text.strip()
+# # print(username)
+# repo_name = a_tags[1].text.strip()
+# # print(repo_name)
+# repo_url = base_url + a_tags[1]['href']
 # print(repo_url)
 
 stars_selector = "Counter js-social-count"
@@ -157,13 +157,17 @@ def get_topic_repos(topic_doc):
 
 
 
-# url7 = topic_URLs[7]
-# topic7_doc = get_topic_page(url7)
-# topic7_repos = get_topic_repos(topic7_doc)
+# # url7 = topic_URLs[7]
+# # topic7_doc = get_topic_page(url7)
+# # topic7_repos = get_topic_repos(topic7_doc)
 
-# print(topic7_repos)
+# # print(topic7_repos)
 
-get_topic_repos(get_topic_page(topic_URLs[4])).to_csv('Android.csv', index = None)
+# get_topic_repos(get_topic_page(topic_URLs[4])).to_csv('Android.csv', index = None)
+def scrape_topic(topic_url, topic_name):
+    topic_df = get_topic_repos(get_topic_page(topic_url))
+    topic_df.to_csv(topic_name + '.csv', index = None)
+
 
 def get_topic_title(doc):
     title_selection = "f3 lh-condensed mb-0 mt-1 Link--primary"
@@ -174,7 +178,21 @@ def get_topic_title(doc):
     return topic_titles
 
 def get_topic_description(doc):
-    
+    description = 'f5 color-fg-muted mb-0 mt-1'
+    topic_descs = doc.find_all('p', class_ = description)
+    topic_descriptions = []
+    for tag in topic_descs:
+        topic_descriptions.append(tag.text.strip())
+    return topic_descriptions
+
+def get_topic_urls(doc):
+    link = "no-underline flex-grow-0"
+    topic_link_tags = doc.find_all('a',class_ =link)
+    topic_URLs = []
+    base_url = "https://github.com"
+    for tag in topic_link_tags:
+        topic_URLs.append(base_url+tag['href'])
+    return topic_URLs
 
 def scrape_topics():
     topics_url = 'https://github.com/topics'
@@ -182,24 +200,23 @@ def scrape_topics():
     if response.status_code != 200:
         raise Exception('Failed to load page {}'.format(topics_url))
     
-    description = 'f5 color-fg-muted mb-0 mt-1'
-    topic_description = doc.find_all('p', class_ = description)
+    topics_dictionary = {
+        'title': get_topic_title(doc),
+        'description': get_topic_description(doc),
+        'url': get_topic_urls(doc)
+    }
 
-    link = "no-underline flex-grow-0"
-    topic_link_tags = doc.find_all('a',class_ =link)
+    return pd.DataFrame(topics_dictionary)
 
-    
-
-    topic_desc = []
-    for tag in topic_description:
-        topic_desc.append(tag.text.strip())
-
-    topic_URLs = []
-    base_url = "https://github.com"
-    for tag in topic_link_tags:
-        topic_URLs.append(base_url+tag['href'])
-
-
+# print(scrape_topics())
 
 
 def scrape_topics_repos():
+    print('Scraping list of topics')
+    topics_df = scrape_topics()
+    for index, row in topics_df.iterrows():
+        print('Scraping top repositories for "{}"'.format(row['title']))
+        scrape_topic(row['url'], row['title'])
+
+
+print(scrape_topics_repos())
